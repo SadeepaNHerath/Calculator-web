8c7b406c0752884a2c4619903ab16fa2cb28b88d
SadeepaNHerath <sadeepahearth@gmail.com>
2024-12-04 22:53:42 +0530

    updated

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-function calc() {
-    let number1 = new Number(document.getElementById("num1").value);
-    let number2 = new Number(document.getElementById("num2").value);
-    let operator = document.getElementById("operator").value;
-    let output = document.getElementById("output");
-
-    console.log(operator);
-    
-    switch (operator) {
-        case "+": output.innerHTML = "Answer is " + (number1 + number2); break;
-        case "-": output.innerHTML = "Answer is " + (number1 - number2); break;
-        case "*": output.innerHTML = "Answer is " + (number1 * number2); break;
-        case "/": output.innerHTML = "Answer is " + (number1 / number2); break;
-        default: output.innerHTML = "Choose option";
+function appendToDisplay(value) {
+    document.calc.txt.value += value;
+  }
+  
+  function clearAll() {
+    document.calc.txt.value = '';
+  }
+  
+  function calculateResult() {
+    try {
+      document.calc.txt.value = eval(document.calc.txt.value);
+    } catch (e) {
+      document.calc.txt.value = "Error";
     }
-
-} 
+  }
+  
+  document.addEventListener('keydown', function (e) {
+    const validKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '/', '*', '-', '+'];
+    
+    if (validKeys.includes(e.key)) {
+      appendToDisplay(e.key);
+    } else if (e.key === 'Enter') {
+      calculateResult();
+    } else if (e.key === 'Backspace') {
+      document.calc.txt.value = document.calc.txt.value.slice(0, -1);
+    }
+  });
+  
